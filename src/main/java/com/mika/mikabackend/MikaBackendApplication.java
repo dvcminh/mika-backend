f221f3bea420613fb944534b459fa77b4fde1a95
Vũ Đức Minh <vuducminh210503@gmail.com>
2024-03-13 09:34:45 +0700

    init project

---
+++ src/main/java/com/mika/mikabackend/MikaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mika.mikabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikaBackendApplication.class, args);
+	}
+
+}
